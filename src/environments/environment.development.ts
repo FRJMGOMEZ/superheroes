9633c3f42341d692156cffa1507214e0e3790c9d
francisco.gomez@ineco.es <francisco.gomez@ineco.com>
2024-05-17 13:36:31 +0200

    configuración de firestore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig : {
+      apiKey: "AIzaSyCi-M4L-ooglll5oxXPVFsgCpdG-GGfaWk",
+      authDomain: "superheroes-81061.firebaseapp.com",
+      projectId: "superheroes-81061",
+      storageBucket: "superheroes-81061.appspot.com",
+      messagingSenderId: "1059091301042",
+      appId: "1:1059091301042:web:af8fa917c6a8c4e1a7ffab",
+      measurementId: "G-3K5SD06L00"
+      }
+};
